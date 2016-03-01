a8e80525461b01b406830de5397107c252bcd279
Александр Лунев <isuntc@gmail.com>
2016-03-01 12:17:41 +0800

    Base styles

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+const cx = require('classnames/bind').bind(require('./App.css'));
 
 export default class App extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className={cx('app')}>
         App
       </div>
     );
